d883f146cc7dea7c725187ec1713e8af6da09ffd
smartful <remi_rodrigues@hotmail.com>
2024-02-14 14:01:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import ejs from "ejs";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+let spendings = [];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    title: "Budget",
+    spendings,
+  });
+});
+
+app.post("/add-spending", (req, res) => {
+  const { description, price, category } = req.body;
+
+  const newSpending = {
+    description,
+    price,
+    category,
+  };
+
+  spendings.push(newSpending);
+  res.render("spending-table", { spendings });
+});
+
+app.listen(PORT, () => {
+  console.log(`The server in listening at PORT : ${PORT}`);
+});
